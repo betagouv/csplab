6039ee9059551a17dd5593fe02c6b720ff93824b
vincent porte <ljp-studio@protonmail.com>
2026-02-04 18:50:18 +0100

    ♻️(backend) reuse settings file for servers and tests

--- src/tycho/start.sh
+++ src/tycho/start.sh
@@ -1,23 +1,7 @@
 #!/usr/bin/env bash
 
-# We may expect that the following environment variables are set:
-#
-#   - PORT: the server port
-#   - TYCHO_DEBUG: activate the debug mode (for development) [*]
-#
-# [*] optional
-
 set -euo pipefail
 
-declare -i debug=${TYCHO_DEBUG:-0}
-
-
-if [ ${debug} == 1 ]; then
-  echo "⚗️ DEBUG mode activated. We hope your are not running in production. 🤞"
-  echo "🚀 Starting development server"
-  python3 manage.py runserver "0.0.0.0:${PORT}"
-else
-  echo "PRODUCTION mode activated."
-  echo "🚀 Starting server"
-  gunicorn config.wsgi:application --log-file -
-fi
+echo "PRODUCTION mode activated."
+echo "🚀 Starting server"
+gunicorn config.wsgi:application --log-file -
