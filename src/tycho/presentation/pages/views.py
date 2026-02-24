2428f071643631e0689d28f6eb32b8c5d8644613
ShallowRed <lucaspoulain2@gmail.com>
2026-02-24 18:49:47 +0100

    ✨(candidate) create error static page

--- src/tycho/presentation/pages/views.py
+++ src/tycho/presentation/pages/views.py
@@ -1,9 +1,27 @@
-"""Home page view."""
+from http import HTTPStatus
 
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    """Landing page view for CSPLab."""
-
     template_name = "pages/home.html"
+
+
+def custom_403_view(
+    request: HttpRequest, exception: Exception | None = None
+) -> HttpResponse:
+    return render(request, "403.html", status=HTTPStatus.FORBIDDEN)
+
+
+def custom_404_view(
+    request: HttpRequest, exception: Exception | None = None
+) -> HttpResponse:
+    return render(request, "404.html", status=HTTPStatus.NOT_FOUND)
+
+
+def custom_500_view(
+    request: HttpRequest, exception: Exception | None = None
+) -> HttpResponse:
+    return render(request, "500.html", status=HTTPStatus.INTERNAL_SERVER_ERROR)
