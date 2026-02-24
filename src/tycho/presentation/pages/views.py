3c982a416af9f0d39804cc50471caa1863a6af14
ShallowRed <lucaspoulain2@gmail.com>
2026-02-24 18:48:30 +0100

    ✨(candidate) add terms static page

--- src/tycho/presentation/pages/views.py
+++ src/tycho/presentation/pages/views.py
@@ -7,3 +7,9 @@ class HomeView(TemplateView):
     """Landing page view for CSPLab."""
 
     template_name = "pages/home.html"
+
+
+class TermsView(TemplateView):
+    """Terms and conditions page."""
+
+    template_name = "pages/terms.html"
