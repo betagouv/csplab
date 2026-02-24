10923993e27ceba154364af502eac9b331388d8f
ShallowRed <lucaspoulain2@gmail.com>
2026-02-24 18:45:13 +0100

    ✨(candidate) add privacy static page

--- src/tycho/presentation/pages/views.py
+++ src/tycho/presentation/pages/views.py
@@ -7,3 +7,9 @@ class HomeView(TemplateView):
     """Landing page view for CSPLab."""
 
     template_name = "pages/home.html"
+
+
+class PrivacyView(TemplateView):
+    """Privacy policy page."""
+
+    template_name = "pages/privacy.html"
