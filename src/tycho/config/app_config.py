40c54b53966044575a08c0607dde9822db93f6e6
Atheane <eroyant@gmail.com>
2026-03-04 12:31:03 +0100

    ✨(tycho) add albert embedding service

--- src/tycho/config/app_config.py
+++ src/tycho/config/app_config.py
@@ -66,6 +66,8 @@ class AppConfig(BaseModel):
 
     # Vector Database
     vector_db_type: Literal["PGVECTOR", "QDRANT"]
+    # Embedding
+    embedding_type: Literal["ALBERT", "OPENAI"]
 
     # OpenAI/OpenRouter
     openrouter_api_key: str
@@ -103,6 +105,7 @@ class AppConfig(BaseModel):
         return cls(
             ocr_type=settings.OCR_TYPE,
             vector_db_type=settings.VECTOR_DB_TYPE,
+            embedding_type=settings.EMBEDDING_TYPE,
             openrouter_api_key=settings.OPENROUTER_API_KEY,
             openrouter_base_url=settings.OPENROUTER_BASE_URL,
             openrouter_embedding_model=settings.OPENROUTER_EMBEDDING_MODEL,
