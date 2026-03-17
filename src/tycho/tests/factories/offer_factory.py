4e9136b1a0cc42614ee2a95c8c1908f06080dc14
vincent porte <ljp-studio@protonmail.com>
2026-03-17 17:04:08 +0100

    ♻️(shared) align Offer factory to entity domain definition

--- src/tycho/tests/factories/offer_factory.py
+++ src/tycho/tests/factories/offer_factory.py
@@ -3,9 +3,16 @@ from typing import List, Optional
 from uuid import uuid4
 
 from django.utils import timezone
+from pydantic import HttpUrl
 
+from domain.entities.offer import Offer
 from domain.value_objects.category import Category
 from domain.value_objects.contract_type import ContractType
+from domain.value_objects.country import Country
+from domain.value_objects.department import Department
+from domain.value_objects.limit_date import LimitDate
+from domain.value_objects.localisation import Localisation
+from domain.value_objects.region import Region
 from domain.value_objects.verse import Verse
 from infrastructure.django_apps.shared.models.offer import OfferModel
 
@@ -21,9 +28,10 @@ class OfferFactory:
         verse: Optional[Verse] = None,
         category: Optional[Category] = None,
         contract_type: Optional[ContractType] = None,
-        offer_url: Optional[str] = None,
+        offer_url: Optional[HttpUrl] = None,
         publication_date: Optional[datetime] = None,
-        beginning_date: Optional[datetime] = None,
+        beginning_date: Optional[LimitDate] = None,
+        localisation: Optional[Localisation] = None,
         country: Optional[str] = None,
         region: Optional[str] = None,
         department: Optional[str] = None,
@@ -31,6 +39,7 @@ class OfferFactory:
         processing: bool = False,
         processed_at: Optional[datetime] = None,
         archived_at: Optional[datetime] = None,
+        save_in_db: Optional[bool] = True,
     ) -> OfferModel:
         if external_id is None:
             external_id = (
@@ -58,20 +67,25 @@ class OfferFactory:
         if archived_at:
             archived_at = timezone.make_aware(archived_at)
 
-        offer = OfferModel(
+        if localisation is None and country and region and department:
+            localisation = Localisation(
+                country=Country(country),
+                region=Region(code=region),
+                department=Department(code=department),
+            )
+
+        offer = Offer(
             id=uuid4(),
             external_id=external_id,
-            verse=verse.value if verse else None,
-            title=title,
-            profile=profile,
-            mission=mission,
-            category=category.value if category else None,
-            contract_type=contract_type.value if contract_type else None,
-            organization=organization,
+            verse=verse,
+            title=title or "Test Offer Title",
+            profile=profile or "Test profile description",
+            mission=mission or "Test mission description",
+            category=category,
+            contract_type=contract_type,
+            organization=organization or "Test Organization",
             offer_url=offer_url,
-            country=country,
-            region=region,
-            department=department,
+            localisation=localisation,
             publication_date=publication_date,
             beginning_date=beginning_date,
             processing=processing,
@@ -79,15 +93,18 @@ class OfferFactory:
             archived_at=archived_at,
         )
 
-        offer.save()
+        offer_model = OfferModel.from_entity(offer)
 
-        if updated_at:
-            OfferModel.objects.filter(id=offer.id).update(
-                updated_at=timezone.make_aware(updated_at)
-            )
-            offer.refresh_from_db()
+        if save_in_db:
+            offer_model.save()
 
-        return offer
+            if updated_at:
+                OfferModel.objects.filter(id=offer.id).update(
+                    updated_at=timezone.make_aware(updated_at)
+                )
+                offer_model.refresh_from_db()
+
+        return offer_model
 
     @staticmethod
     def create_batch(
