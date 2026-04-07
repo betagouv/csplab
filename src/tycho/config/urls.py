9690e2a80998a498fbc35b157de704a1fca2b1ad
vincent porte <ljp-studio@protonmail.com>
2026-04-07 13:25:46 +0200

    ♻️(tycho) fix url concatenation type error
    [*urlpatterns] returns list[URLResolver], while debug_toolbar_urls() returns list[URLPattern | URLResolver]. [*urlpatterns, *debug_toolbar_urls()] lets mypy unify the element types into the wider union.

--- src/tycho/config/urls.py
+++ src/tycho/config/urls.py
@@ -1,12 +1,10 @@
-"""URL configuration for tycho project."""
-
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import URLPattern, URLResolver, include, path
 
 from presentation.api import urls as api_urls
 
-urlpatterns = [
+urlpatterns: list[URLPattern | URLResolver] = [
     path("", include("presentation.pages.urls")),
     path("api/", include(api_urls)),
     path("admin/", admin.site.urls),
@@ -17,7 +15,7 @@ urlpatterns = [
 if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
     from debug_toolbar.toolbar import debug_toolbar_urls
 
-    urlpatterns = [*urlpatterns] + debug_toolbar_urls()
+    urlpatterns = [*urlpatterns, *debug_toolbar_urls()]
 
 if settings.DEBUG and "django_browser_reload" in settings.INSTALLED_APPS:
     urlpatterns.append(path("__reload__/", include("django_browser_reload.urls")))
