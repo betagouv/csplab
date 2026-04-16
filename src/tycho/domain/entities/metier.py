3edbcbe0d12cfb23cb489973423800e92812998b
Atheane <eroyant@gmail.com>
2026-04-16 15:37:05 +0200

    ✨(tycho-ingestion) add metiers interfaces to domain layer

--- src/tycho/domain/entities/metier.py
+++ src/tycho/domain/entities/metier.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import Dict, List, Optional
 from uuid import UUID, uuid4
 
 from domain.interfaces.entity_interface import IEntity
 from domain.value_objects.competence import Competence
+from domain.value_objects.verse import Verse
 
 
 @dataclass
@@ -13,4 +14,6 @@ class Metier(IEntity):
     competences: List[Competence]
     domaine_fonctionnel: UUID
     activites: List[str]
+    versant: List[Verse]
+    competences_par_versant: Optional[Dict[Verse, Optional[str]]] = None
     id: UUID = field(default_factory=uuid4)
