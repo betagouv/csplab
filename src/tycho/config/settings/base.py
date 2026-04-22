6a808de2acdc1fc916a7e11ae48ff14cd9290ac4
vincent porte <ljp-studio@protonmail.com>
2026-04-22 13:53:26 +0200

    ✨(api) setup throttle rates

--- src/tycho/config/settings/base.py
+++ src/tycho/config/settings/base.py
@@ -285,6 +285,15 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "5/minute",
+        "user": "120/minute",
+    },
 }
 
 SIMPLE_JWT = {
