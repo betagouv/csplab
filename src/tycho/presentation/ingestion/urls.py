34841afb14694e8b181389efe50aff48035fd122
vincent porte <ljp-studio@protonmail.com>
2026-04-23 17:24:15 +0200

    🎨(ingestion-presentation) fix url names

--- src/tycho/presentation/ingestion/urls.py
+++ src/tycho/presentation/ingestion/urls.py
@@ -5,6 +5,6 @@ from presentation.ingestion.views import ConcoursUploadView
 
 app_name = "ingestion"
 urlpatterns = [
-    path("concours/upload/", ConcoursUploadView.as_view(), name="concours-upload"),
-    path("health/huey/", HueyHealthView.as_view(), name="health-huey"),
+    path("concours/upload/", ConcoursUploadView.as_view(), name="concours_upload"),
+    path("health/huey/", HueyHealthView.as_view(), name="health_huey"),
 ]
