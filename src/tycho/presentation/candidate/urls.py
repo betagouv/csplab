e00f9e8c91c46d71dae75e2b1421cfd878fcbd9e
Atheane <eroyant@gmail.com>
2026-04-23 12:03:51 +0200

    ♻️(candidate-presentation) delete unused search corps

--- src/tycho/presentation/candidate/urls.py
+++ src/tycho/presentation/candidate/urls.py
@@ -1,8 +1,5 @@
-"""URL configuration for candidate app."""
-
 from django.urls import path
 
-from presentation.candidate.views.corps_search import CorpsSearchView
 from presentation.candidate.views.cv_flow import (
     ConcoursDrawerView,
     CVResultsView,
@@ -13,7 +10,6 @@ from presentation.candidate.views.cv_flow import (
 app_name = "candidate"
 
 urlpatterns = [
-    path("", CorpsSearchView.as_view(), name="index"),
     path("cv-upload/", CVUploadView.as_view(), name="cv_upload"),
     path(
         "cv/<uuid:cv_uuid>/results/",
