7ecfcdebb22774dc14512f66aa2fac983276364e
vincent porte <ljp-studio@protonmail.com>
2026-05-12 17:22:51 +0200

    ♻️(ingestion-usecase) drop unused ListOffersResult class

--- src/web/application/ingestion/interfaces/list_offers_input.py
+++ src/web/application/ingestion/interfaces/list_offers_input.py
@@ -1,7 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Optional
-
-from pydantic import BaseModel
+from typing import Optional
 
 from domain.entities.offer import Offer
 from domain.interfaces.page_interface import IPage
@@ -11,13 +9,6 @@ from domain.interfaces.page_interface import IPage
 class GetFilteredOffersInput:
     active: bool
     external_id_contains: Optional[str]
-
-
-class ListOffersResult(BaseModel):
-    offers: List[Offer]
-
-    class Config:
-        arbitrary_types_allowed = True
 
 
 class ListOffersQuerySetResult:
