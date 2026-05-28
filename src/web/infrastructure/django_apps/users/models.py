60954b129d5e0a0c4636e1d1d33f3567af2e9293
vincent porte <ljp-studio@protonmail.com>
2026-05-28 16:13:42 +0200

    ⚡️(users-infrastructure) make email as username field

--- src/web/infrastructure/django_apps/users/models.py
+++ src/web/infrastructure/django_apps/users/models.py
@@ -3,5 +3,15 @@ from django.db import models
 
 
 class UserModel(AbstractUser):
+    email = models.EmailField(unique=True, null=False, blank=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
     class Meta:
         db_table = "auth_user"
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.email
