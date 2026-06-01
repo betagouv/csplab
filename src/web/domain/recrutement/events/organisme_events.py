60cb6eb49aad6d901ccc6f9fbeabd9d03543d17d
Atheane <eroyant@gmail.com>
2026-06-01 08:52:35 +0200

    ✨(ats-domain) add events for organisme in recrutement context

---
+++ src/web/domain/recrutement/events/organisme_events.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+from domain.ddd.domain_event import DomainEvent
+from domain.recrutement.value_objects.etapes_recrutement import EtapesRecrutement
+
+
+@dataclass(frozen=True)
+class OrganismeParametresInitialises(DomainEvent):
+    parametres: EtapesRecrutement
+
+
+@dataclass(frozen=True)
+class OrganismeParametresModifies(DomainEvent):
+    parametres: EtapesRecrutement
