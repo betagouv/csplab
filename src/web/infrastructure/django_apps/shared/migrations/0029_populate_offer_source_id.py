69fbe08022d5e8a1f95e011d17e07d45f49c9ebc
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2026-06-02 16:02:58 +0200

    ✨(web-infrastructure) backfill source_id for offers (#643)
    * ✨(web-infrastructure) backfill source_id for offers
    
    * ♻️(web) make elidable=True the migration

---
+++ src/web/infrastructure/django_apps/shared/migrations/0029_populate_offer_source_id.py
@@ -0,0 +1,16 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("shared", "0028_offermodel_source_id"),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="UPDATE offers SET source_id = (SELECT source_id FROM sources LIMIT 1)",
+            reverse_sql=migrations.RunSQL.noop,
+            elidable=True,
+        ),
+    ]
