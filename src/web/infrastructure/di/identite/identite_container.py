d4a6240870021324ab5eaa89c1d9ce3bcac8e3ca
vincent porte <ljp-studio@protonmail.com>
2026-06-11 18:43:17 +0200

    ✨(identite-usecase) add create_agent

--- src/web/infrastructure/di/identite/identite_container.py
+++ src/web/infrastructure/di/identite/identite_container.py
@@ -1,7 +1,11 @@
 from dependency_injector import containers, providers
 
+from application.identite.usecases.create_agent import CreateAgentUsecase
 from application.identite.usecases.get_utilisateur_details import (
     GetUtilisateurDetailUsecase,
+)
+from infrastructure.repositories.identite.postgres_agent_repository import (
+    PostgresAgentRepository,
 )
 from infrastructure.repositories.identite.postgres_utilisateur_repository import (
     PostgresUtilisateurRepository,
@@ -13,8 +17,15 @@ class IdentiteContainer(containers.DeclarativeContainer):
     logger_service: providers.Dependency = providers.Dependency()
 
     postgres_utilisateur_repository = providers.Singleton(PostgresUtilisateurRepository)
+    postgres_agent_repository = providers.Singleton(PostgresAgentRepository)
 
     get_utilisateur_details_usecase = providers.Factory(
         GetUtilisateurDetailUsecase,
+        utilisateur_repository=postgres_utilisateur_repository,
+    )
+
+    create_agent_usecase = providers.Factory(
+        CreateAgentUsecase,
+        agent_repository=postgres_agent_repository,
         utilisateur_repository=postgres_utilisateur_repository,
     )
