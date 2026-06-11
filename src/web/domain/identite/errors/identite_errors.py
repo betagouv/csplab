3448b8722fc63bd9fccebd0bec28646403ef6c81
vincent porte <ljp-studio@protonmail.com>
2026-06-11 18:40:29 +0200

    ✨(identite-infrastructure) add get_by_email and create in Utilisateur repository

--- src/web/domain/identite/errors/identite_errors.py
+++ src/web/domain/identite/errors/identite_errors.py
@@ -6,3 +6,8 @@ from ddd.domain_errors import DomainError
 class UtilisateurDoesNotExist(DomainError):
     def __init__(self, entity_id: UUID | str):
         super().__init__(f"User with ID {entity_id} does not exist")
+
+
+class UtilisateurAlreadyExists(DomainError):
+    def __init__(self, entity_id: UUID | str):
+        super().__init__(f"User with ID {entity_id} already exists")
