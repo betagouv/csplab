e45c307bca150ab92adef5c94ed40b51d55eac7b
smdsgn <smdsgn@users.noreply.github.com>
2026-06-11 12:56:45 +0000

    Deploying to gh-pages from @ betagouv/csplab@bc321422377538f3d678a75fdf5459611a46b90a 🚀

---
+++ storybook/pr-740/assets/CspBadge.stories-DECq41Sk.js
@@ -0,0 +1,236 @@
+import{l as q,h as u,j as A,a1 as E,g as R,e as P,W as D,D as j,A as Z,b as K,K as m}from"./vue.esm-bundler-7zVN4DZj.js";import{I as O}from"./iconify-DRloO12f.js";import{_ as W}from"./_plugin-vue_export-helper-DlAUqK2U.js";const $={key:0},Y={class:"badge__label"},T=q({__name:"CspBadge",props:{size:{default:"md"},variant:{default:"default"},label:{},type:{},icon:{},color:{}},setup(e){const s=e,c=K(()=>{if(s.type)switch(s.type){case"info":return"ri:information-fill";case"new":return"ri:flashlight-fill";case"warning":return"ri:alert-fill";case"error":return"ri:spam-fill";case"success":return"ri:checkbox-circle-fill"}return s.icon?s.icon:null});return(G,H)=>(m(),u("p",{class:Z(["badge",[`badge--${e.variant}`,`badge--${e.size}`,{[`badge--type-${e.type}`]:!!e.type},{"badge--custom-color":!!e.color}]]),style:j({color:e.color??void 0})},[c.value?(m(),u("span",$,[A(E(O),{icon:c.value,width:12,height:12,"aria-hidden":"true",class:"badge__icon"},null,8,["icon"])])):R("",!0),P("span",Y,D(s.label),1)],6))}}),n=W(T,[["__scopeId","data-v-5f0b3cc0"]]);T.__docgenInfo={exportName:"default",displayName:"CspBadge",type:1,props:[{name:"key",global:!0,description:"",tags:[],required:!1,type:"PropertyKey",declarations:[],schema:{kind:"enum",type:"PropertyKey",schema:["string","number","symbol"]}},{name:"ref",global:!0,description:"",tags:[],required:!1,type:"VNodeRef",declarations:[],schema:{kind:"enum",type:"VNodeRef",schema:["string","Ref<any, any>",{kind:"event",type:"(ref: Element | ComponentPublicInstance<{}, {}, {}, {}, {}, {}, {}, {}, false, ComponentOptionsBase<any, any, any, any, any, any, any, any, any, {}, {}, string, {}, {}, {}, string, ComponentProvideOptions>, ... 4 more ..., any>, refs: Record<...>): void"}]}},{name:"ref_for",global:!0,description:"",tags:[],required:!1,type:"boolean",declarations:[],schema:{kind:"enum",type:"boolean",schema:["false","true"]}},{name:"ref_key",global:!0,description:"",tags:[],required:!1,type:"string",declarations:[],schema:"string"},{name:"class",global:!0,description:"",tags:[],required:!1,type:"unknown",declarations:[],schema:"unknown"},{name:"style",global:!0,description:"",tags:[],required:!1,type:"unknown",declarations:[],schema:"unknown"}],events:[],slots:[],exposed:[],sourceFiles:"/home/runner/work/csplab/csplab/src/web/presentation/frontend/src/components/base/CspBadge/CspBadge.vue"};const U={title:"Éléments/Génériques/CspBadge",component:n,tags:["autodocs"],parameters:{controls:{include:["variant","size","label","type","icon","color"]},docs:{description:{component:"Badge générique pour afficher des statuts ou états"}}},argTypes:{variant:{control:{type:"radio"},options:["default","soft","outline"],description:"Variant de style du badge.",table:{type:{summary:"default | soft | outline"},defaultValue:{summary:"default"}}},size:{control:{type:"radio"},options:["sm","md","lg"],description:"Taille du badge.",table:{type:{summary:"sm | md | lg"},defaultValue:{summary:"md"}}},label:{control:{type:"text"},description:"Texte visible à l'intérieur du badge.",table:{type:{summary:"string"}}},type:{control:{type:"radio"},options:["info","success","new","warning","error"],description:"Type de badge préconfiguré avec des couleurs et icônes par défaut. Ne peut pas être utilisé conjointement avec les props `icon` ou `color`.",table:{type:{summary:"info | success | new | warning | error"}}},icon:{control:{type:"text"},description:'Icone personnalisée à afficher à côté du label. Doit être une référence d\'icône compatible avec le composant `CspIcon` (ex: "ri:settings-3-line"). Ne peut pas être utilisé conjointement avec les props `type` ou `color`.',table:{type:{summary:"string"}}},color:{control:{type:"text"},description:'Couleur personnalisée pour le badge. Peut être n\'importe quelle valeur de couleur CSS valide (ex: "red", "#ff0000", "rgb(255, 0, 0)"). Ne peut pas être utilisé conjointement avec la prop `type`.',table:{type:{summary:"string"}}},default:{control:!1,table:{disable:!0}},class:{control:!1,table:{disable:!0}},style:{control:!1,table:{disable:!0}},key:{control:!1,table:{disable:!0}},ref:{control:!1,table:{disable:!0}},ref_for:{control:!1,table:{disable:!0}},ref_key:{control:!1,table:{disable:!0}}},args:{variant:"default",size:"md",label:"Libellé badge"},render:e=>({components:{CspBadge:n},setup(){return{args:e}},template:'<CspBadge v-bind="args" />'})},p=["default","soft","outline"],d=["sm","md","lg"],F=["info","success","new","warning","error"],a={args:{label:"Libellé badge"}},r={render:e=>({components:{CspBadge:n},setup(){return{variants:p,args:e}},template:`
+      <div class="flex gap-12 flex-wrap">
+      <div
+        v-for="v in variants"
+        :key="v"
+      >
+        <p class="mb-2">{{ v }}</p>
+        <CspBadge
+          v-bind="args"
+          :variant="v"
+          label="Libellé badge"
+        />
+      </div>
+      </div>
+    `})},t={render:e=>({components:{CspBadge:n},setup(){return{sizes:d,args:e}},template:`
+      <div class="flex gap-12 flex-wrap">
+        <div
+          v-for="s in sizes"
+          :key="s"
+        >
+          <p class="mb-2">{{ s }}</p>
+          <CspBadge
+            v-bind="args"
+            :size="s"
+            label="Libellé badge"
+          />
+        </div>
+      </div>
+    `})},o={render:e=>({components:{CspBadge:n},setup(){return{sizes:d,args:e}},template:`
+      <div class="flex gap-12 flex-wrap">
+        <div
+          v-for="s in sizes"
+          :key="s"
+        >
+          <p class="mb-2">{{ s }}</p>
+          <CspBadge
+            v-bind="args"
+            :size="s"
+            label="Libellé badge"
+            icon="ri:settings-3-line"
+          />
+        </div>
+      </div>
+    `})},l={render:e=>({components:{CspBadge:n},setup(){return{variants:p,sizes:d,args:e}},template:`
+      <div class="flex flex-col gap-12">
+        <div
+          v-for="v in variants"
+          :key="v"
+        >
+          <p class="mb-2">{{ v }}</p>
+          <div class="flex gap-8 flex-wrap">
+            <CspBadge
+              v-for="s in sizes"
+              :key="s"
+              v-bind="args"
+              :variant="v"
+              :size="s"
+              label="Libellé badge"
+              color="purple"
+            />
+          </div>
+        </div>
+      </div>
+    `})},i={render:e=>({components:{CspBadge:n},setup(){return{variants:p,types:F,args:e}},template:`
+      <div class="flex gap-12 flex-wrap">
+        <div
+          v-for="v in variants"
+          :key="v"
+        >
+          <p class="mb-2">{{ v }}</p>
+          <div class="flex gap-8 flex-wrap">
+            <CspBadge
+              v-for="t in types"
+              :key="t"
+              v-bind="args"
+              :variant="v"
+              :type="t"
+              label="Libellé badge"
+            />
+          </div>
+        </div>
+      </div>
+    `})};var v,g,f;a.parameters={...a.parameters,docs:{...(v=a.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    label: 'Libellé badge'
+  }
+}`,...(f=(g=a.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};var b,y,x;r.parameters={...r.parameters,docs:{...(b=r.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      CspBadge
+    },
+    setup() {
+      return {
+        variants: VARIANTS,
+        args
+      };
+    },
+    template: \`
+      <div class="flex gap-12 flex-wrap">
+      <div
+        v-for="v in variants"
+        :key="v"
+      >
+        <p class="mb-2">{{ v }}</p>
+        <CspBadge
+          v-bind="args"
+          :variant="v"
+          label="Libellé badge"
+        />
+      </div>
+      </div>
+    \`
+  })
+}`,...(x=(y=r.parameters)==null?void 0:y.docs)==null?void 0:x.source}}};var C,k,B;t.parameters={...t.parameters,docs:{...(C=t.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      CspBadge
+    },
+    setup() {
+      return {
+        sizes: SIZES,
+        args
+      };
+    },
+    template: \`
+      <div class="flex gap-12 flex-wrap">
+        <div
+          v-for="s in sizes"
+          :key="s"
+        >
+          <p class="mb-2">{{ s }}</p>
+          <CspBadge
+            v-bind="args"
+            :size="s"
+            label="Libellé badge"
+          />
+        </div>
+      </div>
+    \`
+  })
+}`,...(B=(k=t.parameters)==null?void 0:k.docs)==null?void 0:B.source}}};var w,z,h;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      CspBadge
+    },
+    setup() {
+      return {
+        sizes: SIZES,
+        args
+      };
+    },
+    template: \`
+      <div class="flex gap-12 flex-wrap">
+        <div
+          v-for="s in sizes"
+          :key="s"
+        >
+          <p class="mb-2">{{ s }}</p>
+          <CspBadge
+            v-bind="args"
+            :size="s"
+            label="Libellé badge"
+            icon="ri:settings-3-line"
+          />
+        </div>
+      </div>
+    \`
+  })
+}`,...(h=(z=o.parameters)==null?void 0:z.docs)==null?void 0:h.source}}};var S,I,_;l.parameters={...l.parameters,docs:{...(S=l.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      CspBadge
+    },
+    setup() {
+      return {
+        variants: VARIANTS,
+        sizes: SIZES,
+        args
+      };
+    },
+    template: \`
+      <div class="flex flex-col gap-12">
+        <div
+          v-for="v in variants"
+          :key="v"
+        >
+          <p class="mb-2">{{ v }}</p>
+          <div class="flex gap-8 flex-wrap">
+            <CspBadge
+              v-for="s in sizes"
+              :key="s"
+              v-bind="args"
+              :variant="v"
+              :size="s"
+              label="Libellé badge"
+              color="purple"
+            />
+          </div>
+        </div>
+      </div>
+    \`
+  })
+}`,...(_=(I=l.parameters)==null?void 0:I.docs)==null?void 0:_.source}}};var N,V,L;i.parameters={...i.parameters,docs:{...(N=i.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      CspBadge
+    },
+    setup() {
+      return {
+        variants: VARIANTS,
+        types: TYPES,
+        args
+      };
+    },
+    template: \`
+      <div class="flex gap-12 flex-wrap">
+        <div
+          v-for="v in variants"
+          :key="v"
+        >
+          <p class="mb-2">{{ v }}</p>
+          <div class="flex gap-8 flex-wrap">
+            <CspBadge
+              v-for="t in types"
+              :key="t"
+              v-bind="args"
+              :variant="v"
+              :type="t"
+              label="Libellé badge"
+            />
+          </div>
+        </div>
+      </div>
+    \`
+  })
+}`,...(L=(V=i.parameters)==null?void 0:V.docs)==null?void 0:L.source}}};const X=["Default","Variants","Sizes","CustomIcon","CustomColor","WithType"];export{l as CustomColor,o as CustomIcon,a as Default,t as Sizes,r as Variants,i as WithType,X as __namedExportsOrder,U as default};
