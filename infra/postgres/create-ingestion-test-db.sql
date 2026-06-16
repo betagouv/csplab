01f03e632a2b256f52646c41a93238898e44c2dc
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2026-06-16 12:55:42 +0200

    ♻️(ingestion) avoid hardcoding test database URL and recreate test DB from scratch (#785)
    * ♻️(ingestion) avoid hardcoding test database URL and recreate test DB from scratch
    
    * ♻️(ingestion) add default value for TEST_DATABASE_URL in test-ingestion target

--- infra/postgres/create-ingestion-test-db.sql
+++ infra/postgres/create-ingestion-test-db.sql
@@ -1,6 +1,5 @@
--- Creates the ingestion_test database.
--- Idempotent: safe to run multiple times.
+-- Creates the ingestion_test database, dropping it first if it exists.
 -- The ingestion user (created by create-ingestion-db.sql) is reused as owner.
 
-SELECT 'CREATE DATABASE ingestion_test OWNER ingestion'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'ingestion_test')\gexec
+DROP DATABASE IF EXISTS ingestion_test;
+CREATE DATABASE ingestion_test OWNER ingestion;
