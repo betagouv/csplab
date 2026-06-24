46d7a050b4ac4603bce42fce16ceeeadf6027026
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2026-06-24 10:16:27 +0200

    🐛(ingestion) corrige la SourceFactory pour générer des sources TALENTSOFT valides

--- src/ingestion/tests/factories/domain_factories.py
+++ src/ingestion/tests/factories/domain_factories.py
@@ -1,11 +1,17 @@
 from polyfactory.factories.dataclass_factory import DataclassFactory
 from referentiel.entities.source import Source
+from referentiel.value_objects.source_type import SourceType
 
 from domain.entities.webhook import Webhook
 
 
 class SourceFactory(DataclassFactory[Source]):
     __model__ = Source
+    type = SourceType.TALENTSOFT
+    client_id_front = "client_front"
+    client_id_back = "client_back"
+    base_url_front = "https://front.example.com"
+    base_url_back = "https://back.example.com"
 
 
 class WebhookFactory(DataclassFactory[Webhook]):
