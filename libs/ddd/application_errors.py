2c8e7e9aa0f953a8e0ad8626490d76f3826a6d7b
Atheane <eroyant@gmail.com>
2026-06-26 19:04:45 +0200

    ✨(lib-ddd) move application error in lib

---
+++ libs/ddd/application_errors.py
@@ -0,0 +1,20 @@
+class ApplicationError(Exception):
+    def __init__(
+        self,
+        message: str,
+        details: dict | None = None,
+        status_code: int | None = None,
+    ):
+        self.message = message
+        self.details = details or {}
+        self.status_code = status_code
+        super().__init__(self.message)
+
+    @property
+    def error_type(self) -> str:
+        classes = []
+        for cls in self.__class__.__mro__:
+            if cls is Exception:
+                break
+            classes.append(cls.__name__)
+        return "::".join(reversed(classes))
