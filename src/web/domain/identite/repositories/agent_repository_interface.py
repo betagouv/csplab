73878742c5d6eab227f17be3052518060704726f
Atheane <eroyant@gmail.com>
2026-06-26 14:38:28 +0200

    ✨(recruteur) need to add get_by_ids in agent repository

--- src/web/domain/identite/repositories/agent_repository_interface.py
+++ src/web/domain/identite/repositories/agent_repository_interface.py
@@ -1,4 +1,5 @@
 from typing import Protocol
+from uuid import UUID
 
 from domain.identite.entities.agent import Agent
 from domain.identite.entities.utilisateurs import Utilisateur
@@ -7,3 +8,4 @@ from domain.identite.entities.utilisateurs import Utilisateur
 class IAgentRepository(Protocol):
     def create(self, utilisateur: Utilisateur, agent: Agent) -> Agent: ...
     def get_by_email(self, email: str) -> Agent | None: ...
+    def get_by_ids(self, ids: list[UUID]) -> list[Agent]: ...
--- src/web/infrastructure/repositories/identite/postgres_agent_repository.py
+++ src/web/infrastructure/repositories/identite/postgres_agent_repository.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from domain.identite.entities.agent import Agent
 from domain.identite.entities.utilisateurs import Utilisateur
 from domain.identite.repositories.agent_repository_interface import IAgentRepository
@@ -13,6 +15,12 @@ class PostgresAgentRepository(IAgentRepository):
             return profil.to_entity()
         except ProfilAgentModel.DoesNotExist:
             return None
+
+    def get_by_ids(self, ids: list[UUID]) -> list[Agent]:
+        profils = ProfilAgentModel.objects.select_related("utilisateur").filter(
+            utilisateur__username__in=[str(id) for id in ids]
+        )
+        return [profil.to_entity() for profil in profils]
 
     def create(self, utilisateur: Utilisateur, agent: Agent) -> Agent:
         ProfilAgentModel.from_entity(utilisateur, agent).save()
