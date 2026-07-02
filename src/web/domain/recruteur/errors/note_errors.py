55b2b6e5531a3712f34fc397f1b0e33a946281d5
vincent porte <ljp-studio@protonmail.com>
2026-07-02 18:53:25 +0200

    🎨(recruteur-domain) yagni

--- src/web/domain/recruteur/errors/note_errors.py
+++ src/web/domain/recruteur/errors/note_errors.py
@@ -16,13 +16,3 @@ class CandidatureIntrouvable(NoteError):
     def __init__(self, candidature_id: UUID):
         self.candidature_id = candidature_id
         super().__init__(f"La candidature {candidature_id} est introuvable")
-
-
-class NoteModificationNonAutorisee(NoteError):
-    def __init__(self, note_id: UUID):
-        super().__init__(f"La note {note_id} ne peut être modifiée que par son auteur")
-
-
-class NoteSuppressionNonAutorisee(NoteError):
-    def __init__(self, note_id: UUID):
-        super().__init__(f"La note {note_id} ne peut être supprimée que par son auteur")
