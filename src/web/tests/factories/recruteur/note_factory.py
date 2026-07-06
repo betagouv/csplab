0c283b92b5ae2dad803eae2bdfe3a05a2447c034
vincent porte <ljp-studio@protonmail.com>
2026-07-06 16:47:51 +0200

    ⚡️(recruteur-tooling) set candidature and agent in note_factory if not passed

--- src/web/tests/factories/recruteur/note_factory.py
+++ src/web/tests/factories/recruteur/note_factory.py
@@ -5,6 +5,8 @@ from faker import Faker
 from domain.recruteur.entities.note import Note
 from infrastructure.django_apps.recruteur.models import NoteModel
 from infrastructure.mappers.note_mapper import NoteMapper
+from tests.factories.candidate.candidature_factory import CandidatureFactory
+from tests.factories.identite.agent_factory import AgentFactory
 
 fake = Faker("fr_FR")
 
@@ -24,10 +26,15 @@ class NoteFactory:
 
     @staticmethod
     def create_model(
-        candidature_id: UUID,
-        publie_par_id: UUID,
+        candidature_id: UUID | None = None,
+        publie_par_id: UUID | None = None,
         message: str | None = None,
     ) -> NoteModel:
+        if candidature_id is None:
+            candidature_id = CandidatureFactory.build_model().id
+        if publie_par_id is None:
+            publie_par_id = UUID(AgentFactory.create_model().utilisateur_id)
+
         note = NoteFactory.create_entity(
             candidature_id=candidature_id,
             publie_par_id=publie_par_id,
