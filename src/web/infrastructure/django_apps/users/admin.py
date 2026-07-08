87f277c8c660174ae62c1cfc559957c242ca3640
Vincent Porte <ljp-studio@protonmail.com>
2026-07-08 16:50:30 +0200

    🎨(identite-presentation) get better admin readibility on profil (#938)
    add ProfilAgent and ProfilCandidat admin, update __str__ methods

--- src/web/infrastructure/django_apps/users/admin.py
+++ src/web/infrastructure/django_apps/users/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from infrastructure.django_apps.users.models import UserModel
+from infrastructure.django_apps.users.models import (
+    ProfilAgentModel,
+    ProfilCandidatModel,
+    UserModel,
+)
+from infrastructure.django_apps.utils.admin import ReadOnlyAdminMixin
 
 
 @admin.register(UserModel)
@@ -17,3 +22,17 @@ class UserModelAdmin(admin.ModelAdmin):
     search_fields = ("email",)
     readonly_fields = ["username", "id", "date_joined", "last_login", "password"]
     filter_horizontal = ("sources",)
+
+
+@admin.register(ProfilCandidatModel)
+class ProfilCandidatAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ("utilisateur", "created_at", "updated_at")
+    search_fields = ("utilisateur__email",)
+    date_hierarchy = "created_at"
+
+
+@admin.register(ProfilAgentModel)
+class ProfilAgentAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ("utilisateur", "intitule_poste", "created_at", "updated_at")
+    search_fields = ("utilisateur__email", "intitule_poste")
+    date_hierarchy = "created_at"
