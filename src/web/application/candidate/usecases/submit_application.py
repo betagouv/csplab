92fe49bf70b7deb60d1928aff44776c3993b620b
Atheane <eroyant@gmail.com>
2026-07-09 16:32:20 +0200

    ✨(candidature-application) update submit application usecase

--- src/web/application/candidate/usecases/submit_application.py
+++ src/web/application/candidate/usecases/submit_application.py
@@ -5,16 +5,16 @@ from application.candidate.commands.submit_application_command import (
     SubmitApplicationCommand,
 )
 from domain.candidate.entities.candidature import Candidature
-from domain.candidate.exceptions.candidature_errors import (
-    CandidatureNexistePas,
-)
 from domain.candidate.repositories.candidature_repository_interface import (
     ICandidatureRepository,
 )
-from domain.candidate.services.candidature_actors_validator import (
-    CandidatureActorsValidator,
-)
 from domain.commons.services.audit_log_writer import AuditLogWriter
+from domain.identite.repositories.candidat_repository_interface import (
+    ICandidatRepository,
+)
+from domain.recruteur.repositories.recrutement_repository_interface import (
+    IRecrutementRepository,
+)
 
 
 class SubmitApplicationUsecase(
@@ -22,46 +22,35 @@ class SubmitApplicationUsecase(
 ):
     def __init__(
         self,
+        candidat_repository: ICandidatRepository,
         candidature_repository: ICandidatureRepository,
-        actors_validator: CandidatureActorsValidator,
+        recrutement_repository: IRecrutementRepository,
         audit_log_writer: AuditLogWriter,
         logger: ILogger,
     ):
         self.logger = logger
+        self.candidat_repository = candidat_repository
         self.candidature_repository = candidature_repository
-        self.actors_validator = actors_validator
+        self.recrutement_repository = recrutement_repository
         self.audit_log_writer = audit_log_writer
 
     def execute(self, command: SubmitApplicationCommand) -> Candidature:
         self.logger.info(
             "Starting candidature submission for candidate_uuid='%s', offer_uuid=%d",
         )
-        offre_id = command.offre_id
-        candidat_id = command.candidat_id
+        # todo: rbac should manage access and existence:
+        self.candidat_repository.get_by_id(command.candidat_id)
+        self.recrutement_repository.get_by_id(command.offre_id)
 
-        self.actors_validator.validate(candidat_id, offre_id)
-
-        # todo: rbac should manage access:
-        # candidate can only get access to its candidatures
-        candidature = None
-        try:
-            candidature = self.candidature_repository.get_by_offer(
-                offre_id, candidat_id
-            )
-        except CandidatureNexistePas as e:
-            self.logger.info(e.message)
-            candidature = Candidature.create(
-                offre_id=offre_id,
-                candidat_id=candidat_id,
-            )
-        # todo entity document
-        # add document gate way and repository here
-        # candidature.deposer_documents(DocumentsDeposes())
-
+        candidature = Candidature.create(
+            offre_id=command.offre_id,
+            candidat_id=command.candidat_id,
+        )
         candidature.soumettre_candidature()
+
         self.candidature_repository.save(candidature)
 
         self.audit_log_writer.drain_events(
-            utilisateur_id=candidat_id, aggregate=candidature
+            utilisateur_id=command.candidat_id, aggregate=candidature
         )
         return candidature
--- src/web/domain/candidate/repositories/candidature_repository_interface.py
+++ src/web/domain/candidate/repositories/candidature_repository_interface.py
@@ -3,10 +3,6 @@ from uuid import UUID
 
 from ddd.base_repository_interface import IBaseRepository
 
-from domain.candidate.entities.candidature import Candidature
-
 
 class ICandidatureRepository(IBaseRepository, Protocol):
-    def get_by_offer(self, offer_id: UUID, candidate_id: UUID) -> Candidature: ...
-
     def exists(self, candidature_id: UUID) -> bool: ...
