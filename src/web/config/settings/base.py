149df73f68bb03757d595b2575a3f6313b63b3bb
Vincent Porte <ljp-studio@protonmail.com>
2026-07-21 15:31:23 +0200

    ⚡️(ingestion-presentation) remove browsable api rendered option in DRF (#1004)

--- src/web/config/settings/base.py
+++ src/web/config/settings/base.py
@@ -330,6 +330,9 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
