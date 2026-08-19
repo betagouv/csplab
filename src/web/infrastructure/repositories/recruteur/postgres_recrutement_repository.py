6fa6c6f2cea65129870abff3464fe695716e4f6e
Atheane <eroyant@gmail.com>
2026-08-19 20:29:14 +0200

    ✨(recruteur-infrastructure) init etapes recrutement

--- src/web/infrastructure/repositories/recruteur/postgres_recrutement_repository.py
+++ src/web/infrastructure/repositories/recruteur/postgres_recrutement_repository.py
@@ -1,12 +1,14 @@
 from uuid import UUID
 
 from django.core.exceptions import ObjectDoesNotExist
+from django.db import transaction
 
 from domain.recruteur.entities.recrutement import Recrutement
 from domain.recruteur.errors.recrutement_errors import RecrutementInexistant
 from domain.recruteur.repositories.recrutement_repository_interface import (
     IRecrutementRepository,
 )
+from infrastructure.django_apps.recruteur.models.etape import EtapeModel
 from infrastructure.django_apps.recruteur.models.recrutement import RecrutementModel
 from infrastructure.mappers.recrutement_mapper import RecrutementMapper
 
@@ -23,3 +25,30 @@ class PostgresRecrutementRepository(IRecrutementRepository):
             return self.mapper.to_domain(model)
         except ObjectDoesNotExist as e:
             raise RecrutementInexistant(aggregate_id) from e
+
+    def save(self, recrutement: Recrutement) -> None:
+        with transaction.atomic():
+            recrutement_model = RecrutementModel.objects.select_for_update().get(
+                offre_id=recrutement.entity_id
+            )
+            recrutement_model.ordre_etapes = self.mapper.from_domain(recrutement.etapes)
+            recrutement_model.save()
+
+            EtapeModel.objects.bulk_create(
+                [
+                    EtapeModel(
+                        id=etape.entity_id,
+                        recrutement_id=recrutement.entity_id,
+                        categorie=etape.categorie.value,
+                        nom=etape.nom,
+                    )
+                    for etape in recrutement.etapes
+                ],
+                update_conflicts=True,
+                update_fields=["categorie", "nom"],
+                unique_fields=["id"],
+            )
+
+            EtapeModel.objects.filter(recrutement_id=recrutement.entity_id).exclude(
+                id__in=[etape.entity_id for etape in recrutement.etapes]
+            ).delete()
--- src/web/tests/infrastructure/recruteur/test_init_recrutement_etapes.py
+++ src/web/tests/infrastructure/recruteur/test_init_recrutement_etapes.py
@@ -1,10 +1,27 @@
 from unittest.mock import Mock
+from uuid import UUID, uuid4
 
 import pytest
 
+from application.recruteur.usecases.init_recrutement_etapes import (
+    InitRecrutementEtapesCommand,
+)
 from config.app_config import AppConfig
 from domain.commons.services.audit_log_writer import AuditLogWriter
+from domain.recruteur.errors.organisme_permission_errors import (
+    AccesOrganismeRefuse,
+    AccesRecrutementRefuse,
+)
+from domain.recruteur.value_objects.roles import (
+    AgentOrganismeRole,
+    AgentRecrutementRole,
+)
 from infrastructure.di.recruteur.recruteur_container import RecruteurContainer
+from infrastructure.factories.identite.organisme_factory import OrganismeFactory
+from infrastructure.factories.recruteur.etapes_recrutement_factory import (
+    EtapeRecrutementFactory,
+)
+from infrastructure.factories.recruteur.recrutement_factory import RecrutementFactory
 from infrastructure.gateways.shared.logger import LoggerService
 
 NB_ETAPES_PAR_DEFAUT = 6
@@ -21,73 +38,70 @@ def recruteur_integration_container_fixture(db):
     return container
 
 
-# class TestInitRecrutementEtapes:
-#     @pytest.mark.parametrize(
-#         "kwargs",
-#         [
-#             {"organisme_role": AgentOrganismeRole.RESPONSABLE},
-#             {
-#                 "organisme_role": AgentOrganismeRole.MEMBRE,
-#                 "agent_role": AgentRecrutementRole.RESPONSABLE,
-#             },
-#         ],
-#         ids=["responsable_organisme", "responsable_recrutement"],
-#     )
-#     def test_init_recrutement_etapes(
-# self, db, recruteur_integration_container, kwargs):
-#         recrutement_model = RecrutementFactory.create_model(**kwargs)
-#         usecase = recruteur_integration_container.init_recrutement_etapes_usecase()
+class TestInitRecrutementEtapes:
+    @pytest.mark.parametrize(
+        "kwargs",
+        [
+            {"organisme_role": AgentOrganismeRole.RESPONSABLE},
+            {
+                "organisme_role": AgentOrganismeRole.MEMBRE,
+                "agent_role": AgentRecrutementRole.RESPONSABLE,
+            },
+        ],
+        ids=["responsable_organisme", "responsable_recrutement"],
+    )
+    def test_init_recrutement_etapes(self, db, recruteur_integration_container, kwargs):
+        # parametres par defaut = une seule etape en cours
+        en_cours = EtapeRecrutementFactory.create_entity()
+        etapes = EtapeRecrutementFactory.create_entity_batch(en_cours=(en_cours,))
 
-#         resultat = usecase.execute(
-#             InitRecrutementEtapesCommand(
-#                 organisme_id=recrutement_model.organisme_id,
-#                 recrutement_id=recrutement_model.offre_id,
-#                 utilisateur_id=recrutement_model.agents_liaisons.get().agent_id,
-#             )
-#         )
+        agent, organisme = OrganismeFactory.create_model_with_agent(
+            role=kwargs.get("organisme_role"), etapes=etapes
+        )
+        recrutement_model = RecrutementFactory.create_model(
+            organisme_id=organisme.id,
+            agent_id=UUID(agent.utilisateur_id),
+            agent_role=kwargs.get("agent_role"),
+            persist_etapes=False,
+        )
+        usecase = recruteur_integration_container.init_recrutement_etapes_usecase()
 
-#         assert len(resultat) == NB_ETAPES_PAR_DEFAUT
+        resultat = usecase.execute(
+            InitRecrutementEtapesCommand(
+                organisme_id=recrutement_model.organisme_id,
+                recrutement_id=recrutement_model.offre_id,
+                utilisateur_id=UUID(agent.utilisateur_id),
+            )
+        )
 
-#     @pytest.mark.parametrize(
-#         "agent_role",
-#         [AgentRecrutementRole.RECRUTEUR, AgentRecrutementRole.CONTRIBUTEUR],
-#     )
-#     def test_denied_agents(self, db, recruteur_integration_container, agent_role):
-#         recrutement_model = RecrutementFactory.create_model(agent_role=agent_role)
-#         usecase = recruteur_integration_container.init_recrutement_etapes_usecase()
+        assert resultat == etapes
 
-#         with pytest.raises(AccesRecrutementRefuse):
-#             usecase.execute(
-#                 InitRecrutementEtapesCommand(
-#                     organisme_id=recrutement_model.organisme_id,
-#                     recrutement_id=recrutement_model.offre_id,
-#                     utilisateur_id=recrutement_model.agents_liaisons.get().agent_id,
-#                 )
-#             )
+    @pytest.mark.parametrize(
+        "agent_role",
+        [AgentRecrutementRole.RECRUTEUR, AgentRecrutementRole.CONTRIBUTEUR],
+    )
+    def test_denied_agents(self, db, recruteur_integration_container, agent_role):
+        recrutement_model = RecrutementFactory.create_model(agent_role=agent_role)
+        usecase = recruteur_integration_container.init_recrutement_etapes_usecase()
 
-#     def test_agents_without_role(self, db, recruteur_integration_container):
-#         recrutement_model = RecrutementFactory.create_model()
-#         usecase = recruteur_integration_container.init_recrutement_etapes_usecase()
+        with pytest.raises(AccesRecrutementRefuse):
+            usecase.execute(
+                InitRecrutementEtapesCommand(
+                    organisme_id=recrutement_model.organisme_id,
+                    recrutement_id=recrutement_model.offre_id,
+                    utilisateur_id=recrutement_model.agents_liaisons.get().agent_id,
+                )
+            )
 
-#         with pytest.raises(AccesOrganismeRefuse):
-#             usecase.execute(
-#                 InitRecrutementEtapesCommand(
-#                     organisme_id=recrutement_model.organisme_id,
-#                     recrutement_id=recrutement_model.offre_id,
-#                     utilisateur_id=uuid4(),
-#                 )
-#             )
+    def test_agents_without_role(self, db, recruteur_integration_container):
+        recrutement_model = RecrutementFactory.create_model()
+        usecase = recruteur_integration_container.init_recrutement_etapes_usecase()
 
-#     def test_init_recrutement_etapes_raises_when_organisme_introuvable(
-#         self, db, recruteur_integration_container
-#     ):
-#         usecase = recruteur_integration_container.init_recrutement_etapes_usecase()
-
-#         with pytest.raises(OrganismeNexistePas):
-#             usecase.execute(
-#                 InitRecrutementEtapesCommand(
-#                     organisme_id=uuid4(),
-#                     recrutement_id=uuid4(),
-#                     utilisateur_id=uuid4(),
-#                 )
-#             )
+        with pytest.raises(AccesOrganismeRefuse):
+            usecase.execute(
+                InitRecrutementEtapesCommand(
+                    organisme_id=recrutement_model.organisme_id,
+                    recrutement_id=recrutement_model.offre_id,
+                    utilisateur_id=uuid4(),
+                )
+            )
