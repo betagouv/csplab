ee1e2f6a918ff649e61f15ca6120dc3b7ad0a2a7
Lucas Poulain <lucaspoulain2@gmail.com>
2026-08-20 11:04:38 +0200

    ✨(ats-presentation) add organisme creation from the admin list

---
+++ src/web/presentation/frontend/src/features/organismes/constants/organisme.ts
@@ -0,0 +1,9 @@
+import type { Versant } from '../types'
+
+export const VERSANT_LABELS: Record<Versant, string> = {
+  FPE: 'Fonction Publique d\'État',
+  FPT: 'Fonction Publique Territoriale',
+  FPH: 'Fonction Publique Hospitalière',
+}
+
+export const SIRET_LENGTH = 14
