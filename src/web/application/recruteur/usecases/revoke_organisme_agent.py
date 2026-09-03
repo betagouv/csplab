f2849b0f5cacd1013ef554a8e9bcc12a3095aff8
Vincent Porte <ljp-studio@protonmail.com>
2026-09-03 13:56:51 +0200

    ⚡️(recruteur) add AuditLogWriter log action calls to the organisme member attach/update/revoke usecases (#1309)

--- src/web/application/recruteur/usecases/revoke_organisme_agent.py
+++ src/web/application/recruteur/usecases/revoke_organisme_agent.py
@@ -3,6 +3,7 @@ from datetime import UTC, datetime
 from typing import cast
 from uuid import UUID
 
+from ddd.entity import Entity
 from ddd.usecase_interface import IUsecase
 
 from application.recruteur.dtos.agent_organisme_read_models import (
@@ -11,6 +12,7 @@ from application.recruteur.dtos.agent_organisme_read_models import (
 from application.recruteur.services.organisme_agent_query_service_interface import (
     IOrganismeAgentQueryService,
 )
+from domain.commons.services.audit_log_writer import AuditLogWriter
 from domain.identite.entities.utilisateurs import Utilisateur
 from domain.identite.services.organisme_permission_service import (
     OrganismePermissionService,
@@ -36,10 +38,12 @@ class RevokeOrganismeAgentUsecase(
         organisme_agent_repository: IOrganismeAgentRepository,
         organisme_agent_query_service: IOrganismeAgentQueryService,
         organisme_permission_service: OrganismePermissionService,
+        audit_log_writer: AuditLogWriter,
     ):
         self.organisme_agent_repository = organisme_agent_repository
         self.organisme_agent_query_service = organisme_agent_query_service
         self.organisme_permission_service = organisme_permission_service
+        self.audit_log_writer = audit_log_writer
 
     def execute(self, command: RevokeOrganismeAgentCommand) -> AgentOrganismeReadModel:
         self.organisme_permission_service.est_autorise(
@@ -51,6 +55,12 @@ class RevokeOrganismeAgentUsecase(
             organisme_id=command.organisme_id,
             agent_id=command.agent_id,
             date_revocation=datetime.now(UTC),
+        )
+        self.audit_log_writer.log_action(
+            utilisateur_id=command.utilisateur.entity_id,
+            entity=Entity(entity_id=command.agent_id),
+            ressource_kind="AgentOrganisme",
+            event_name="AgentOrganismeRoleRevoque",
         )
         agent_organisme = self.organisme_agent_query_service.get_one(
             organisme_id=command.organisme_id, agent_id=command.agent_id
