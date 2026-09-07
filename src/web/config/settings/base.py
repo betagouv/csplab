32d3a952edc361e15ae4a6dd56d947affbecf38f
Lucas Poulain <lucaspoulain2@gmail.com>
2026-09-07 10:37:27 +0200

    ✨(identite) redirect user to the login page after logout

--- src/web/config/settings/base.py
+++ src/web/config/settings/base.py
@@ -177,7 +177,7 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = "users.UserModel"
 LOGIN_URL = "identite:login"
 LOGIN_REDIRECT_URL = "ats:ats_base"
-LOGOUT_REDIRECT_URL = "pages:home"
+LOGOUT_REDIRECT_URL = "identite:login"
 
 AUTHENTICATION_BACKENDS = [
     "infrastructure.authentication.proconnect_backend.ProconnectBackend",
