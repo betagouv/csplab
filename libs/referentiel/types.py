fe9185a29ae4704c5505f081dae4ac6e2f6187fb
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2026-09-08 15:23:49 +0200

    ♻️(ingestion) simplifie SupprimerOrganismesUsecase
    Retire la dépendance à OrganismeSupprime/IDeleteResult (referentiel)
    et aux interfaces ddd (IUsecase, ILogger) : usecase instancié
    directement dans la vue plutôt que via le container, sans logger
    injecté. Relock uv.lock web/ingestion sur referentiel 0.2.9.

--- libs/referentiel/types.py
+++ libs/referentiel/types.py
@@ -16,11 +16,6 @@ class IUpsertResult(TypedDict):
     errors: List[IUpsertError]
 
 
-class IDeleteResult(TypedDict):
-    deleted: int
-    not_found: List[dict[str, str]]
-
-
 # bound=Entity force objgeneric type to be inherited from Entity
 # covariant=True allow for IBatchUpdate[T, SpecificError]
 # and IBatchUpdate[T, GenericError]
--- src/web/application/ingestion/usecases/supprimer_organismes.py
+++ src/web/application/ingestion/usecases/supprimer_organismes.py
@@ -1,7 +1,3 @@
-from ddd.services.logger_interface import ILogger
-from ddd.usecase_interface import IUsecase
-from referentiel.types import IDeleteResult
-
 from application.ingestion.interfaces.supprimer_organismes_input import (
     SupprimerOrganismesInput,
 )
@@ -10,16 +6,14 @@ from domain.identite.repositories.organisme_repository_interface import (
 )
 
 
-class SupprimerOrganismesUsecase(IUsecase[SupprimerOrganismesInput, IDeleteResult]):
+class SupprimerOrganismesUsecase:
     def __init__(
         self,
         organisme_repository: IOrganismeRepository,
-        logger: ILogger,
     ):
         self.organisme_repository = organisme_repository
-        self.logger = logger
 
-    def execute(self, input_data: SupprimerOrganismesInput) -> IDeleteResult:
+    def execute(self, input_data: SupprimerOrganismesInput):
         pairs = [(data.referentiel, data.external_id) for data in input_data.organismes]
         organismes_by_pair = (
             self.organisme_repository.get_by_referentiel_and_external_id_batch(pairs)
@@ -34,13 +28,7 @@ class SupprimerOrganismesUsecase(IUsecase[SupprimerOrganismesInput, IDeleteResul
                     {"referentiel": referentiel, "external_id": external_id}
                 )
                 continue
-            organisme.supprimer()
             found_organismes.append(organisme)
 
         deleted = self.organisme_repository.supprimer_batch(found_organismes)
-        self.logger.info(
-            "SupprimerOrganismes: deleted=%d not_found=%d",
-            deleted,
-            len(not_found),
-        )
         return {"deleted": deleted, "not_found": not_found}
