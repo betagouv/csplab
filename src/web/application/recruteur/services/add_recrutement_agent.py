7793d2c6f8bacc930858157ca66954502b7f31d5
vincent porte <ljp-studio@protonmail.com>
2026-09-11 15:08:01 +0200

    ♻️(recruteur) use recrutement_agent_context_service into list-add-update-revoke agent services

--- src/web/application/recruteur/services/add_recrutement_agent.py
+++ src/web/application/recruteur/services/add_recrutement_agent.py
@@ -3,33 +3,20 @@ from uuid import UUID, uuid4
 from ddd.entity import Entity
 from django.db import transaction
 
+from application.recruteur.context_services.recrutement_agent_context_service import (
+    RecrutementAgentContextService,
+    can_execute,
+)
 from domain.commons.services.audit_log_writer import AuditLogWriter
 from domain.identite.entities.utilisateurs import Utilisateur
 from domain.identite.errors.agent_errors import ProfilAgentNexistePas
-from domain.identite.services.organisme_permission_service import (
-    OrganismePermissionService,
-)
 from domain.identite.value_objects.organisme_action import OrganismeAction
-from domain.recruteur.errors.organisme_agent_errors import AgentNonRattache
-from domain.recruteur.errors.recrutement_agent_errors import AgentDejaMembreRecrutement
-from domain.recruteur.errors.recrutement_errors import RecrutementInexistant
-from infrastructure.django_apps.recruteur.models.organisme import OrganismeAgentModel
 from infrastructure.django_apps.recruteur.models.recrutement import (
     RecrutementAgentModel,
-    RecrutementModel,
 )
 from infrastructure.django_apps.users.models import ProfilAgentModel
 from infrastructure.repositories.commons.postgres_audit_log_repository import (
     PostgresAuditLogRepository,
-)
-from infrastructure.repositories.recruteur.postgres_organisme_agent_repository import (
-    PostgresOrganismeAgentRepository,
-)
-from infrastructure.repositories.recruteur.postgres_organisme_repository import (
-    PostgresOrganismeRecruteurRepository,
-)
-from infrastructure.repositories.recruteur.postgres_recrutement_agent_repository import (  # noqa: E501
-    PostgresRecrutementAgentRepository,
 )
 
 
@@ -41,13 +28,7 @@ def add_recrutement_agent(
     role: str,
     utilisateur: Utilisateur,
 ) -> RecrutementAgentModel:
-    # TODO : to refactor in ADR-009 style once OrganismePermissionService is migrated
-    organisme_permission_service = OrganismePermissionService(
-        organisme_recruteur_repository=PostgresOrganismeRecruteurRepository(),
-        organisme_agent_repository=PostgresOrganismeAgentRepository(),
-        recrutement_agent_repository=PostgresRecrutementAgentRepository(),
-    )
-    organisme_permission_service.est_autorise(
+    can_execute(
         action=OrganismeAction.ADD_RECRUTEMENT_AGENT,
         utilisateur=utilisateur,
         organisme_id=organisme_id,
@@ -58,25 +39,12 @@ def add_recrutement_agent(
     ).exists():
         raise ProfilAgentNexistePas(agent_id)
 
-    if not RecrutementModel.objects.filter(
-        pk=recrutement_id, organisme_id=organisme_id
-    ).exists():
-        raise RecrutementInexistant(recrutement_id)
-
-    agent_rattache_a_organisme = OrganismeAgentModel.objects.filter(
-        organisme_id=organisme_id,
-        agent_id=agent_id,  # type: ignore[misc]
-        date_revocation__isnull=True,
-    ).exists()
-    if not agent_rattache_a_organisme:
-        raise AgentNonRattache(organisme_id, agent_id)
-
-    if RecrutementAgentModel.objects.filter(
-        recrutement_id=recrutement_id,
-        agent_id=agent_id,  # type: ignore[misc]
-        date_revocation__isnull=True,
-    ).exists():
-        raise AgentDejaMembreRecrutement(recrutement_id, agent_id)
+    contexte = RecrutementAgentContextService(
+        organisme_id=organisme_id, recrutement_id=recrutement_id
+    )
+    contexte.check_recrutement_belongs_to_organisme()
+    contexte.check_agent_attached_to_organisme(agent_id)
+    contexte.check_agent_not_active_member(agent_id)
 
     with transaction.atomic():
         recrutement_agent, created = RecrutementAgentModel.objects.update_or_create(
--- src/web/application/recruteur/services/update_recrutement_agent.py
+++ src/web/application/recruteur/services/update_recrutement_agent.py
@@ -3,31 +3,18 @@ from uuid import UUID
 from ddd.entity import Entity
 from django.db import transaction
 
+from application.recruteur.context_services.recrutement_agent_context_service import (
+    RecrutementAgentContextService,
+    can_execute,
+)
 from domain.commons.services.audit_log_writer import AuditLogWriter
 from domain.identite.entities.utilisateurs import Utilisateur
-from domain.identite.services.organisme_permission_service import (
-    OrganismePermissionService,
-)
 from domain.identite.value_objects.organisme_action import OrganismeAction
-from domain.recruteur.errors.organisme_agent_errors import AgentNonRattache
-from domain.recruteur.errors.recrutement_agent_errors import AgentNonMembreRecrutement
-from domain.recruteur.errors.recrutement_errors import RecrutementInexistant
-from infrastructure.django_apps.recruteur.models.organisme import OrganismeAgentModel
 from infrastructure.django_apps.recruteur.models.recrutement import (
     RecrutementAgentModel,
-    RecrutementModel,
 )
 from infrastructure.repositories.commons.postgres_audit_log_repository import (
     PostgresAuditLogRepository,
-)
-from infrastructure.repositories.recruteur.postgres_organisme_agent_repository import (
-    PostgresOrganismeAgentRepository,
-)
-from infrastructure.repositories.recruteur.postgres_organisme_repository import (
-    PostgresOrganismeRecruteurRepository,
-)
-from infrastructure.repositories.recruteur.postgres_recrutement_agent_repository import (  # noqa: E501
-    PostgresRecrutementAgentRepository,
 )
 
 
@@ -39,39 +26,18 @@ def update_recrutement_agent(
     role: str,
     utilisateur: Utilisateur,
 ) -> RecrutementAgentModel:
-    # TODO : to refactor in ADR-009 style once OrganismePermissionService is migrated
-    organisme_permission_service = OrganismePermissionService(
-        organisme_recruteur_repository=PostgresOrganismeRecruteurRepository(),
-        organisme_agent_repository=PostgresOrganismeAgentRepository(),
-        recrutement_agent_repository=PostgresRecrutementAgentRepository(),
-    )
-    organisme_permission_service.est_autorise(
+    can_execute(
         action=OrganismeAction.UPDATE_RECRUTEMENT_AGENT,
         utilisateur=utilisateur,
         organisme_id=organisme_id,
     )
 
-    if not RecrutementModel.objects.filter(
-        pk=recrutement_id, organisme_id=organisme_id
-    ).exists():
-        raise RecrutementInexistant(recrutement_id)
-
-    agent_rattache_a_organisme = OrganismeAgentModel.objects.filter(
-        organisme_id=organisme_id,
-        agent_id=agent_id,  # type: ignore[misc]
-        date_revocation__isnull=True,
-    ).exists()
-    if not agent_rattache_a_organisme:
-        raise AgentNonRattache(organisme_id, agent_id)
-
-    try:
-        recrutement_agent = RecrutementAgentModel.objects.get(
-            recrutement_id=recrutement_id,
-            agent_id=agent_id,  # type: ignore[misc]
-            date_revocation__isnull=True,
-        )
-    except RecrutementAgentModel.DoesNotExist as error:
-        raise AgentNonMembreRecrutement(recrutement_id, agent_id) from error
+    contexte = RecrutementAgentContextService(
+        organisme_id=organisme_id, recrutement_id=recrutement_id
+    )
+    contexte.check_recrutement_belongs_to_organisme()
+    contexte.check_agent_attached_to_organisme(agent_id)
+    recrutement_agent = contexte.get_active_member(agent_id)
 
     with transaction.atomic():
         recrutement_agent.role = role
