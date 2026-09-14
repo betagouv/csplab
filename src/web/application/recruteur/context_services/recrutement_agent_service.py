1669834ae72f32542b717780cdf880bc0eef032f
vincent porte <ljp-studio@protonmail.com>
2026-09-14 13:10:59 +0200

    ✨(recruteur) add queryset methods to OrganismeAgent, Recrutement and RecrutementAgent, refacto datas controls into recrutement_agent_service

---
+++ src/web/application/recruteur/context_services/recrutement_agent_service.py
@@ -0,0 +1,45 @@
+from uuid import UUID
+
+from domain.recruteur.errors.organisme_agent_errors import AgentNonRattache
+from domain.recruteur.errors.recrutement_agent_errors import (
+    AgentDejaMembreRecrutement,
+    AgentNonMembreRecrutement,
+)
+from domain.recruteur.errors.recrutement_errors import RecrutementInexistant
+from infrastructure.django_apps.recruteur.models.organisme import OrganismeAgentModel
+from infrastructure.django_apps.recruteur.models.recrutement import (
+    RecrutementAgentModel,
+    RecrutementModel,
+)
+
+
+class RecrutementAgentService:
+    def __init__(self, *, organisme_id: UUID, recrutement_id: UUID) -> None:
+        self.organisme_id = organisme_id
+        self.recrutement_id = recrutement_id
+
+    def check_recrutement_belongs_to_organisme(self) -> None:
+        if not RecrutementModel.objects.by_organisme_and_recrutement(
+            self.organisme_id, self.recrutement_id
+        ).exists():
+            raise RecrutementInexistant(self.recrutement_id)
+
+    def check_agent_attached_to_organisme(self, agent_id: UUID) -> None:
+        if not OrganismeAgentModel.objects.by_organisme_and_agent(
+            self.organisme_id, agent_id
+        ).exists():
+            raise AgentNonRattache(self.organisme_id, agent_id)
+
+    def check_agent_not_active_member(self, agent_id: UUID) -> None:
+        if RecrutementAgentModel.objects.by_recrutement_and_agent(
+            self.recrutement_id, agent_id
+        ).exists():
+            raise AgentDejaMembreRecrutement(self.recrutement_id, agent_id)
+
+    def get_active_member(self, agent_id: UUID) -> RecrutementAgentModel:
+        try:
+            return RecrutementAgentModel.objects.by_recrutement_and_agent(
+                self.recrutement_id, agent_id
+            ).get()
+        except RecrutementAgentModel.DoesNotExist as error:
+            raise AgentNonMembreRecrutement(self.recrutement_id, agent_id) from error
