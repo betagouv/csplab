1b641897c4d83c2e59ac53c9a7da8b440e6aee81
vincent porte <ljp-studio@protonmail.com>
2026-09-20 10:27:05 +0200

    refactor(recruteur): drop DDD domain codes

--- src/web/domain/recruteur/events/note_events.py
+++
@@ -1,5 +0,0 @@
-from dataclasses import dataclass
-
-from ddd.domain_event import DomainEvent
-
-
--- src/web/domain/recruteur/repositories/note_repository_interface.py
+++
@@ -1,8 +0,0 @@
-from typing import Protocol
-
-from ddd.base_repository_interface import IBaseRepository
-
-from domain.recruteur.entities.note import Note
-
-
-class INoteRepository(IBaseRepository[Note], Protocol): ...
