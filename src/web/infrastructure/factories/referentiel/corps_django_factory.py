2166caa63eb2fa0e4b57d4d3491d4d98e69b5810
vincent porte <ljp-studio@protonmail.com>
2026-09-23 12:34:09 +0200

    fix(referentiel): keep CorpsDjangoFactory labels within Label   limits
    fix flakyness in tests

--- src/web/infrastructure/factories/referentiel/corps_django_factory.py
+++ src/web/infrastructure/factories/referentiel/corps_django_factory.py
@@ -2,11 +2,15 @@ from uuid import uuid4
 
 import factory
 from factory.django import DjangoModelFactory
+from faker import Faker
 from referentiel.value_objects.category import Category
+from referentiel.value_objects.label import Label
 from referentiel.value_objects.ministry import Ministry
 
 from infrastructure.django_apps.referentiel.models.corps import CorpsModel
 from infrastructure.factories.datetime_utils import as_aware
+
+fake = Faker()
 
 
 class CorpsDjangoFactory(DjangoModelFactory):
@@ -19,8 +23,10 @@ class CorpsDjangoFactory(DjangoModelFactory):
     category = Category.A.value
     ministry = Ministry.MI.value
     diploma_level = None
-    short_label = factory.Faker("job")
-    long_label = factory.Faker("sentence")
+    short_label = factory.LazyFunction(
+        lambda: fake.job()[: Label.MAX_SHORT_LABEL_LENGTH]
+    )
+    long_label = factory.Faker("text", max_nb_chars=Label.MAX_LONG_LABEL_LENGTH)
     access_modalities = factory.LazyFunction(list)
     processing = False
     processed_at = None
